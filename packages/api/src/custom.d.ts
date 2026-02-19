df6dee1af2aca5a9086b261c288b80a92e1f5edf
野火漫山秋 <126342626+err510@users.noreply.github.com>
2026-02-19 16:37:25 +0800

    编写custom.d.ts文件，用于TypeScript 自动忽略这些第三方模块的类型检查

---
+++ packages/api/src/custom.d.ts
@@ -0,0 +1,9 @@
+// custom.d.ts
+declare module 'highlightjs';
+declare module 'sax';
+declare module '@omnivore/liqe';
+declare module 'sanitize-html';
+declare module 'urlsafe-base64';
+declare module 'luxon';
+declare module 'addressparser';
+declare module 'dompurify';
